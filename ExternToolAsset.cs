5e780b772345c7465317d97189b0b85dc2476aaa
SpadeAce <jingjizhifeng@hotmail.com>
2020-06-07 19:10:53 +0800

    添加 自定义文件打开方式

--- ExternToolAsset.cs
+++ ExternToolAsset.cs
@@ -1,55 +1,77 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-internal class ExternToolAsset : ScriptableObject
+namespace SpadeAce
 {
-    public List<ExternToolItem> items = new List<ExternToolItem>();
-
-    public void Create(string assetPath)
+    internal class ExternToolAsset : ScriptableObject
     {
-        AssetDatabase.CreateAsset(this, assetPath);
-        AssetDatabase.SaveAssets();
-        AssetDatabase.Refresh();
-    }
+        public List<ExternToolItem> items = new List<ExternToolItem>();
 
-    public static ExternToolAsset Load(string assetPath)
-    {
-        return AssetDatabase.LoadAssetAtPath<ExternToolAsset>(assetPath);
-    }
-}
-
-[CustomEditor(typeof(ExternToolAsset))]
-internal class ExternToolAssetInspector : Editor
-{
-    private ExternToolAsset asset;
-
-    private void OnEnable()
-    {
-        if (asset == null)
-            asset = target as ExternToolAsset;
-    }
-
-    public override void OnInspectorGUI()
-    {
-        EditorGUILayout.BeginVertical();
-        int count = asset.items.Count;
-        GUILayout.Label($"数量: {count}");
-        for (int i = 0; i < count; ++i)
+        public void Create(string assetPath)
         {
-            EditorGUILayout.BeginVertical(GUI.skin.box);
-            GUILayout.Label($"扩展名: {asset.items[i].extension}");
-            GUILayout.Label($"程序路径: {asset.items[i].path}");
+            AssetDatabase.CreateAsset(this, assetPath);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+
+        public string GetPath(string extension)
+        {
+            for (int i = 0; i < items.Count; ++i)
+            {
+                if (extension == items[i].extension)
+                    return items[i].path;
+            }
+            return null;
+        }
+
+        public bool Contains(string extension)
+        {
+            for (int i = 0; i < items.Count; ++i)
+            {
+                if (extension == items[i].extension)
+                    return true;
+            }
+            return false;
+        }
+
+        public static ExternToolAsset Load(string assetPath)
+        {
+            return AssetDatabase.LoadAssetAtPath<ExternToolAsset>(assetPath);
+        }
+    }
+
+    [CustomEditor(typeof(ExternToolAsset))]
+    internal class ExternToolAssetInspector : Editor
+    {
+        private ExternToolAsset asset;
+
+        private void OnEnable()
+        {
+            if (asset == null)
+                asset = target as ExternToolAsset;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.BeginVertical();
+            int count = asset.items.Count;
+            GUILayout.Label($"数量: {count}");
+            for (int i = 0; i < count; ++i)
+            {
+                EditorGUILayout.BeginVertical(GUI.skin.box);
+                GUILayout.Label($"扩展名: {asset.items[i].extension}");
+                GUILayout.Label($"程序路径: {asset.items[i].path}");
+                EditorGUILayout.EndVertical();
+            }
             EditorGUILayout.EndVertical();
         }
-        EditorGUILayout.EndVertical();
     }
-}
 
-[System.Serializable]
-internal class ExternToolItem
-{
-    public string extension;
-    public string path;
+    [System.Serializable]
+    internal class ExternToolItem
+    {
+        public string extension;
+        public string path;
+    }
 }
---
+++ ExternToolExtension.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Diagnostics;
+using System.IO;
+
+namespace SpadeAce
+{
+    internal class ExternToolExtension
+    {
+        private static ExternToolAsset _asset;
+        internal static ExternToolAsset asset
+        {
+            get
+            {
+                if (_asset == null)
+                    _asset = ExternToolAsset.Load("Assets/ExternToolExt.asset");
+                return _asset;
+            }
+        }
+        [OnOpenAsset(2)]
+        public static bool OpenFile(int instanceID, int line, int column)
+        {
+            string filePath = AssetDatabase.GetAssetPath(instanceID);
+            string extension = Path.GetExtension(filePath);
+            string exePath = asset.GetPath(extension);
+            if(!string.IsNullOrWhiteSpace(exePath))
+            {
+                filePath = Path.Combine(Directory.GetParent(Application.dataPath).FullName, filePath);
+                Process.Start(exePath, $"\"{filePath}\"");
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
